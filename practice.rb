d3387413241381e372d384dd1e851dd9352ca2ce
Taichi Ozaki <taichi.ozaki1905@gmail.com>
2023-07-26 17:26:46 +0000

    ruby作成

---
+++ practice.rb
@@ -0,0 +1,40 @@
+puts 'hello ruby!'
+
+
+a = 3
+b = 7
+ new_value = a + b
+  puts new_value
+
+array_month = ["1月","2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
+ puts array_month[7]
+ 
+ 
+ hello = "Hello,"
+ name = "Taichi Ozaki"
+ world = "'s World"
+  new_title = hello + name + world
+   puts new_title
+ 
+
+omusubi_tech = "omusubi" 
+ omusubi_tech += "_tech"
+  puts omusubi_tech 
+  
+  
+calender_2023 = {
+"January" => "1月",
+"February" => "2月",
+"March" => "3月",
+"April" => "4月",
+"May" => "5月",
+"June" => "6月",
+"July" => "7月",
+"August" => "8月",
+"September" => "9月",
+"October" => "10月",
+"November" => "11月",
+"December" => "12月"
+}
+# 12月を表示する
+puts calender_2023["December"]
